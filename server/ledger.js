49a97f249faa79a7ee77d33638993520c7a25cca
nicgd <ngdfulo@me.com>
2021-05-05 16:46:24 +0200

    create account and ledger classes (in progress)

---
+++ server/ledger.js
@@ -0,0 +1,12 @@
+class Account {
+    constructor(_name, _balance) {
+        this.name = _name;
+        this.balance = _balance;
+    }
+}
+
+class Ledger {
+    constructor() {
+        
+    }
+}
