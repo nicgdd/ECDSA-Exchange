96bd36c7d9e05a754b92e0803f549926ad40d11c
nicgd <ngdfulo@me.com>
2021-05-07 15:56:28 +0200

    small improvements

--- server/index.js
+++ server/index.js
@@ -23,15 +23,16 @@ const ledger = new Ledger();
 const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
 
-console.log('================');
+console.log('==================');
+console.log('AVAILABLE ACCOUNTS');
 for (let i = 0; i < START_ACCOUNTS; i++) {
   const key = ec.genKeyPair();
   const privateKey = key.getPrivate().toString(16);
   const publicKey = key.getPublic().encode('hex');
   const address = '0x'+publicKey.slice(-40);
-  console.log('');
-  console.log(`Adress (${i}) : ${address}`);
-  console.log(`Private Key (${i}) : ${privateKey}`);
+  console.log(`(${i})`);
+  console.log(`Address : ${address}`);
+  console.log(`Private Key : ${privateKey}`);
 
   // sets a random balance between 50 & 100
   const balance = Math.floor((Math.random() * 50) + 50);
@@ -40,6 +41,7 @@ for (let i = 0; i < START_ACCOUNTS; i++) {
   //// Ledger class not needed for my challenge 1 solution 
   //const newAccount = ledger.addAccount(publicKey, balance);
 }
+console.log('==================');
 console.log('');
 
 
